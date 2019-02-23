ab9a6bad2b2ca83f9dfe9dae5ed8f4365808a8e6
ZKasica <zkasica@mymail.mines.edu>
2019-02-23 16:52:47 -0700

    Add Google map

--- parking-project/src/app/app.component.ts
+++ parking-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'parking-project';
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(40.0097385, -105.243164),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  } 
 }

970f79ffe8757cafbf89324d2532083e17700a4b
ZKasica <zkasica@mymail.mines.edu>
2019-02-24 09:37:44 -0700

    Display graph in more information section

--- parking-project/src/app/app.component.ts
+++ parking-project/src/app/app.component.ts
@@ -7,6 +7,10 @@ declare var io: any;
 import "../../socketio.min.js";
 import { tick } from '@angular/core/testing';
 
+import { Chart } from 'chart.js';
+import * as moment from 'moment';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -73,12 +77,13 @@ export class AppComponent {
     this.map.panTo(this.selectedLot.getCenter());
     this.map.setZoom(16);
 
-
     $('#info-panel').animate({
       bottom: "-340px"
     }, 300, () => {
       $('#info-panel').removeClass('slideDown');
     });
+
+    this.createChart("chart");
   }
 
   displayLotInfo(parkingZone) {
@@ -96,7 +101,7 @@ export class AppComponent {
   createParkingZones() {
     this.parkingZones = new Map<string, ParkingZone>();
 
-    this.parkingZones.set("D Lot", new ParkingZone(this.map, "D Lot", 10, [
+    this.parkingZones.set("D Lot", new ParkingZone(this.map, "D Lot", 40, [
       {lat: 39.74862680238093, lng: -105.22318225421515},
       {lat: 39.74827209694497, lng: -105.22394668378439},
       {lat: 39.748775283184, lng: -105.22432219304648},
@@ -106,7 +111,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("D Lot"));
     });
     
-    this.parkingZones.set("Q Lot",  new ParkingZone(this.map, "Q Lot", 120, [
+    this.parkingZones.set("Q Lot",  new ParkingZone(this.map, "Q Lot", 60, [
       {lat: 39.75027459728276, lng: -105.22575904541935},
       {lat: 39.750014761322326, lng: -105.22640814000096}, 
       {lat: 39.75059217323679, lng: -105.22707332783665}, 
@@ -119,7 +124,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("Q Lot"));
     });
 
-    this.parkingZones.set("CTLM", new ParkingZone(this.map, "CTLM", 160, [
+    this.parkingZones.set("CTLM", new ParkingZone(this.map, "CTLM", 55, [
       {lat: 39.7501214009411, lng: -105.21948697794733},
       {lat: 39.75074417976547, lng: -105.21824779738245}, 
       {lat: 39.750331744087596, lng: -105.21784546603021}, 
@@ -131,7 +136,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("CTLM"));
     });
 
-    this.parkingZones.set("A Lot", new ParkingZone(this.map, "A Lot", 120, [
+    this.parkingZones.set("A Lot", new ParkingZone(this.map, "A Lot", 33, [
       {lat: 39.74626372114325, lng: -105.22201382936964},
       {lat: 39.74544291833426, lng: -105.22200041832457},
       {lat: 39.74513975502509, lng: -105.22250467361937},
@@ -141,7 +146,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("A Lot"));
     });
 
-    this.parkingZones.set("F Lot", new ParkingZone(this.map, "F Lot", 80, [
+    this.parkingZones.set("F Lot", new ParkingZone(this.map, "F Lot", 47, [
       {lat: 39.74627645485167, lng: -105.22190208925389}, 
       {lat: 39.74535904102645, lng: -105.22188038239051}, 
       {lat: 39.74566839034351, lng: -105.22125810989905},
@@ -151,7 +156,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("F Lot"));
     });
 
-    this.parkingZones.set("AA Lot", new ParkingZone(this.map, "AA Lot", 80, [
+    this.parkingZones.set("AA Lot", new ParkingZone(this.map, "AA Lot", 31, [
       {lat: 39.74533730704826, lng: -105.22187432665015}, 
       {lat: 39.74612305184506, lng: -105.2203883828559}, 
       {lat: 39.745580662037675, lng: -105.22040179390098},
@@ -162,7 +167,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("AA Lot"));
     });  
     
-    this.parkingZones.set("J Lot", new ParkingZone(this.map, "J Lot", 80, [
+    this.parkingZones.set("J Lot", new ParkingZone(this.map, "J Lot", 60, [
       {lat: 39.75094610313953, lng: -105.21805249832892},
       {lat: 39.75028208243195, lng: -105.21740876816534},
       {lat: 39.75052541995597, lng: -105.21695279263281},
@@ -173,7 +178,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("J Lot"));
     });  
 
-    this.parkingZones.set("O and N Lots", new ParkingZone(this.map, "O and N Lots", 80, [
+    this.parkingZones.set("O and N Lots", new ParkingZone(this.map, "O and N Lots", 26, [
       {lat: 39.75235298915961, lng: -105.22008189241842},
       {lat: 39.751585874739405, lng: -105.21937915365652},
       {lat: 39.7518642640246, lng: -105.2188722161527}, 
@@ -184,7 +189,7 @@ export class AppComponent {
       this.selectLot(this.parkingZones.get("O and N Lots"));
     });  
 
-    this.parkingZones.set("FF Lot", new ParkingZone(this.map, "FF Lot", 80, [
+    this.parkingZones.set("FF Lot", new ParkingZone(this.map, "FF Lot", 56, [
       {lat: 39.74658311541243, lng: -105.22121118967823},
       {lat: 39.74603454228738, lng: -105.22066401903919},
       {lat: 39.74616653020521, lng: -105.22036897604755},
@@ -195,6 +200,20 @@ export class AppComponent {
     ]));
     this.parkingZones.get("FF Lot").addClickListener((event) => {
       this.selectLot(this.parkingZones.get("FF Lot"));
+    });  
+
+    this.parkingZones.set("E Lot", new ParkingZone(this.map, "E Lot", 40, [
+      {lat: 39.74777789350077, lng: -105.2198107406677}, 
+      {lat: 39.74759641429301, lng: -105.22014333458554},
+      {lat: 39.74785213485707, lng: -105.22039009781491}, 
+      {lat: 39.74791400259291, lng: -105.22090508194577}, 
+      {lat: 39.748010928600635, lng: -105.2209989592613},
+      {lat: 39.748200655710455, lng: -105.22093726845395},
+      {lat: 39.74842750265594, lng: -105.22049202175748}, 
+      {lat: 39.74777789350077, lng: -105.2198107406677}
+    ]));
+    this.parkingZones.get("E Lot").addClickListener((event) => {
+      this.selectLot(this.parkingZones.get("E Lot"));
     });  
   }
 
@@ -299,5 +318,85 @@ export class AppComponent {
       );
     });
     this.registerLotListEvents();
+  }
+
+  createChart(id) {
+    var myChart = new Chart(document.getElementById("myChart"), {
+      type: 'bar',
+      data: {
+          datasets: [{
+            label: "% Parking Lot Utilization",
+              data: [{
+                          x: moment().hours(7),
+                          y: 0.1 * 100
+              }, {
+                          x: moment().hours(8),
+                          y: 0.8 * 100
+              }, {
+                          x: moment().hours(9),
+                          y: 0.98 * 100
+              }, {
+                          x: moment().hours(10),
+                          y: 1 * 100
+              }, {
+                          x: moment().hours(11),
+                          y: 0.95 * 100
+              }, {
+                          x: moment().hours(12),
+                          y: 0.98 * 100
+              }, {
+                          x: moment().hours(13),
+                          y: 0.7 * 100
+              }, {
+                          x: moment().hours(14),
+                          y: 0.8 * 100
+              }, {
+                          x: moment().hours(15),
+                          y: 0.8 * 100
+              }, {
+                          x: moment().hours(16),
+                          y: 0.6 * 100
+              }, {
+                          x: moment().hours(17),
+                          y: 0.5 * 100
+              }, {
+                          x: moment().hours(18),
+                          y: 0.23 * 100
+              }, {
+                          x: moment().hours(19),
+                          y: 0.1 * 100
+              }, {
+                          x: moment().hours(20),
+                          y: 0.05 * 100
+              }],
+              backgroundColor: 
+                  '#2c6ac850', 
+              borderColor: 
+                  '#2c6ac8',
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }],
+              xAxes: [{
+                  type: 'time',
+                  time: {
+                      min: moment().hours(6),
+                      max: moment().hours(21),
+                      unit: 'hour',
+                      displayFormats: {
+                          hour: 'hA'    
+                      },
+                      stepSize: 3
+                  }
+              }]
+          }
+      }
+  });
   }
 }
